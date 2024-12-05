2a7535cfe851239c5df8a8a7e0505c0b893dc200
= <kennylalek@gmail.com>
2024-12-05 18:29:45 +0100

    added styles for title and header

--- lib/views/themes/style_simple/styles.dart
+++ lib/views/themes/style_simple/styles.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'colors.dart';
 
 
-
 //Paragraph text styling
 const paragraphText = TextStyle(
   fontSize: 12,
@@ -26,3 +25,50 @@ const orangeBoldParagraphText = TextStyle(
   color: orange,
 );
 
+
+//Title text style
+const titleText = TextStyle(
+  fontSize: 16,
+  color: darkBlue,
+);
+
+const boldTitleText = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  color: darkBlue,
+);
+
+const orangeTitleText = TextStyle(
+  fontSize: 16,
+  color: orange,
+);
+
+const orangeBoldTitleText = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  color: orange,
+);
+
+
+// Header text style
+const headerText = TextStyle(
+  fontSize: 16,
+  color: darkBlue,
+);
+
+const boldHeaderText = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  color: darkBlue,
+);
+
+const orangeHeaderText = TextStyle(
+  fontSize: 16,
+  color: orange,
+);
+
+const orangeBoldHeaderText = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  color: orange,
+);
